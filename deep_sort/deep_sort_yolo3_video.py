f68297c3faf641e43a5b0cc7573fc6474f8689da
animebing <yanbing_dong@hotmail.com>
2018-06-11 21:54:24 +0800

    online detection using yolo3 for a video file as input

---
+++ deep_sort/deep_sort_yolo3_video.py
@@ -0,0 +1,355 @@
+# vim: expandtab:ts=4:sw=4
+from __future__ import division, print_function, absolute_import
+
+import argparse
+import os, sys
+
+import cv2
+import numpy as np
+import time
+
+from application_util import preprocessing
+from application_util import visualization
+from deep_sort import nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+
+from yolo3.darknet import Darknet
+from yolo3.utils import do_detect
+
+def gather_video_info(video_file):
+    """Gather video information.
+
+    Parameters
+    ----------
+    sequence_dir : str
+        Path to the MOTChallenge sequence directory.
+    detection_file : str
+        Path to the detection file.
+
+    Returns
+    -------
+    Dict
+        A dictionary of the following sequence information:
+        * video_cap: cv2 video capture handle
+        * detections: A numpy array of detections in MOTChallenge format.
+        * groundtruth: A numpy array of ground truth in MOTChallenge format.
+        * image_size: Image size (height, width).
+        * min_frame_idx: Index of the first frame.
+        * max_frame_idx: Index of the last frame.
+
+    """
+
+    cap = cv2.VideoCapture(video_file)
+    if not cap.isOpened():
+        print("the video is not open!")
+        sys.exit(1)
+    video_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    detections = None
+
+    groundtruth = None
+
+    image_size = (height, width)
+    min_frame_idx = 1
+    max_frame_idx = video_len
+
+    update_ms = 1000 / fps
+
+
+    # NOTE: what is feature_dim used for? -> which is appearance feature related to each detection
+    feature_dim = 0
+    seq_info = {
+        "video_cap": cap,
+        "detections": detections,
+        "groundtruth": groundtruth,
+        "image_size": image_size,
+        "min_frame_idx": min_frame_idx,
+        "max_frame_idx": max_frame_idx,
+        "feature_dim": feature_dim,
+        "update_ms": update_ms
+    }
+    return seq_info
+
+
+#def create_detections(detection_mat, frame_idx, image_filename, encoder, min_height=0):
+def create_detections(detection_mat, frame_idx, min_height=0):
+    """Create detections for given frame index from the raw detection matrix.
+
+    Parameters
+    ----------
+    detection_mat : ndarray
+        Matrix of detections. The first 10 columns of the detection matrix are
+        in the standard MOTChallenge detection format. In the remaining columns
+        store the feature vector associated with each detection.
+    frame_idx : int
+        The frame index.
+    min_height : Optional[int]
+        A minimum detection bounding box height. Detections that are smaller
+        than this value are disregarded.
+
+    Returns
+    -------
+    List[tracker.Detection]
+        Returns detection responses at given frame index.
+
+    """
+    frame_indices = detection_mat[:, 0].astype(np.int)
+    mask = frame_indices == frame_idx
+
+    #bgr_image = cv2.imread(image_filename, cv2.IMREAD_COLOR)
+    #boxes = detection_mat[mask][:, 2:6]
+    #features = encoder(bgr_image, boxes.copy())
+
+    detection_list = []
+    #for row, appearance in zip(detection_mat[mask], features):
+    for row in detection_mat[mask]:
+        bbox, confidence, feature = row[2:6], row[6], row[10:]
+        #bbox, confidence, feature = row[2:6], row[6], appearance
+        if bbox[3] < min_height:
+            continue
+        detection_list.append(Detection(bbox, confidence, feature))
+    return detection_list
+
+def create_gts(gt_mat, frame_idx):
+    """Create detections for given frame index from the raw groundtruth detection matrix.
+
+    Parameters
+    ----------
+    gt_mat : ndarray
+        Matrix of ground truth detections
+    frame_idx : int
+        The frame index.
+
+    Returns
+    -------
+    List[detection.Detection]
+        Returns detection responses at given frame index.
+
+    """
+    frame_indices = gt_mat[:, 0].astype(np.int)
+    mask = frame_indices == frame_idx
+
+    detection_list = []
+    #for row, appearance in zip(detection_mat[mask], features):
+    for row in gt_mat[mask]:
+        if int(row[6]) == 0 or int(row[7]) != 1:
+            continue
+        bbox = row[2:6]
+        feature = np.ones((1,), dtype=np.float32)
+        vis = float(row[-1])
+        #print('create gt detections')
+        assert int(row[6]) == 1, 'flag should be 1'
+        #if bbox[3] < min_height:
+        #    continue
+        detection_list.append(Detection(bbox, vis, feature))
+    return detection_list
+
+
+def create_model(cfg_file, weight_file, use_cuda):
+    model = Darknet(cfg_file)
+    model.print_network()
+    model.load_weights(weight_file)
+
+    if use_cuda:
+        model.cuda()
+    return model
+
+
+def create_det_from_model(model, img, conf_thresh, nms_thresh, min_detection_height, use_cuda):
+
+    height, width = img.shape[:2]
+    sized = cv2.resize(img, (model.width, model.height))
+    sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+    boxes = do_detect(model, sized, conf_thresh, nms_thresh, use_cuda)
+
+    dummy_feat = np.ones((1,), dtype=np.float32)
+    detection_list = []
+
+    for i in range(len(boxes)):
+        box = boxes[i]
+        cls_id = box[6]
+        if cls_id != 0:
+            continue
+
+        x = int(round((box[0] - box[2] / 2.0) * width))
+        y = int(round((box[1] - box[3] / 2.0) * height))
+        w = int(box[2] * width)
+        h = int(box[3] * height)
+        if h < min_detection_height:
+            continue
+        cls_conf = box[5]
+
+        detection_list.append(Detection((x, y, w, h), cls_conf, dummy_feat))
+
+    return detection_list
+
+
+def run(video_file, min_confidence,
+        nms_max_overlap, min_detection_height, max_cosine_distance,
+        nn_budget, display):
+    """Run multi-target tracker on a particular sequence.
+
+    Parameters
+    ----------
+    video_file : str
+        Path to the video file.
+    min_confidence : float
+        Detection confidence threshold. Disregard all detections that have
+        a confidence lower than this value.
+    nms_max_overlap: float
+        Maximum detection overlap (non-maxima suppression threshold).
+    min_detection_height : int
+        Detection height threshold. Disregard all detections that have
+        a height lower than this value.
+    max_cosine_distance : float
+        Gating threshold for cosine distance metric (object appearance).
+    nn_budget : Optional[int]
+        Maximum size of the appearance descriptor gallery. If None, no budget
+        is enforced.
+    display : bool
+        If True, show visualization of intermediate tracking results.
+
+    """
+
+    cfg_file = "yolo3/cfg/yolov3.cfg"
+    weight_file = "yolo3/yolov3.weights"
+    #weight_file = 'yolo3/backup/MOT17Det/yolov3-mot17det_10000.weights'
+    use_cuda = 1
+
+
+    det_model = create_model(cfg_file, weight_file, use_cuda)
+
+    seq_info = gather_video_info(video_file)
+    metric = nn_matching.NearestNeighborDistanceMetric(
+        "cosine", max_cosine_distance, nn_budget)
+
+    tracker = Tracker(metric)
+    # just for warming up
+    img = cv2.imread('./000001.jpg')
+
+    sized = cv2.resize(img, (det_model.width, det_model.height))
+    sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+    boxes = do_detect(det_model, sized, 0.5, 0.4, use_cuda)
+
+
+    def frame_callback(vis, frame_idx):
+        #print("Processing frame %05d" % frame_idx)
+        #global total_frames, total_time
+        ret, img = seq_info['video_cap'].read()
+        if not ret:
+            print('there is no frame!')
+            sys.exit(1)
+
+        #time_0 = time.time()
+        # Load image and generate detections.
+        detections = create_det_from_model(det_model, img, 0.5, 0.4, min_detection_height, use_cuda)
+
+        #detections = create_detections(
+        #    seq_info["detections"], frame_idx, min_detection_height)
+        #if seq_info['groundtruth'] is not None:
+        #    gts = create_gts(seq_info['groundtruth'], frame_idx)
+
+        #detections = create_detections(
+        #    seq_info["detections"], frame_idx, seq_info["image_filenames"][frame_idx], encoder, min_detection_height)
+
+
+        detections = [d for d in detections if d.confidence >= min_confidence]
+
+        # Run non-maxima suppression.
+        boxes = np.array([d.tlwh for d in detections])
+        scores = np.array([d.confidence for d in detections])
+        indices = preprocessing.non_max_suppression(
+            boxes, nms_max_overlap, scores)
+        detections = [detections[i] for i in indices]
+
+        # Update tracker.
+        tracker.predict()
+        tracker.update(detections)
+        #time_1 = time.time()
+        #total_time += time_1 - time_0
+        #total_frames += 1
+        # Update visualization.
+        if display:
+            #image = cv2.imread(
+            #    seq_info["image_filenames"][frame_idx], cv2.IMREAD_COLOR)
+            #vis.set_image(image.copy())
+            vis.set_image(img.copy())
+            #vis.draw_detections(detections)
+            #vis.draw_detections(gts)
+            vis.draw_trackers(tracker.tracks)
+
+        # Store results.
+        # NOTE: store from n_init frame(1-based index)
+
+
+        # for track in tracker.tracks:
+        #     # NOTE: the condition is different from that in drawing tracks
+        #     if not track.is_confirmed() or track.time_since_update > 1:
+        #         continue
+        #     # NOTE: store estimated state instead of observation
+        #     bbox = track.to_tlwh()
+        #     results.append([
+        #         frame_idx, track.track_id, bbox[0], bbox[1], bbox[2], bbox[3]])
+
+
+    # Run tracker.
+    if display:
+        visualizer = visualization.Visualization(seq_info, update_ms=1)
+    else:
+        visualizer = visualization.NoVisualization(seq_info)
+    visualizer.run(frame_callback)
+
+    #print(
+    #    "average time for one frame: %.5f, FPS: %.5f" % (total_time / total_frames, total_frames / total_time))
+
+
+def parse_args():
+    """ Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Deep SORT")
+
+    parser.add_argument(
+        "--video_file", help="Path to the video file",
+        default=None, required=True)
+    parser.add_argument(
+        "--min_confidence", help="Detection confidence threshold. Disregard "
+        "all detections that have a confidence lower than this value.",
+        default=0.8, type=float)
+    parser.add_argument(
+        "--min_detection_height", help="Threshold on the detection bounding "
+        "box height. Detections with height smaller than this value are "
+        "disregarded", default=0, type=int)
+    parser.add_argument(
+        "--nms_max_overlap",  help="Non-maxima suppression threshold: Maximum "
+        "detection overlap.", default=1.0, type=float)
+    parser.add_argument(
+        "--max_cosine_distance", help="Gating threshold for cosine distance "
+        "metric (object appearance).", type=float, default=0.2)
+    parser.add_argument(
+        "--nn_budget", help="Maximum size of the appearance descriptors "
+        "gallery. If None, no budget is enforced.", type=int, default=None)
+
+    parser.add_argument(
+        "--display", help="Show intermediate tracking results",
+        action='store_true')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    print(args)
+    run(
+        args.video_file, args.min_confidence,
+        args.nms_max_overlap, args.min_detection_height,
+        args.max_cosine_distance, args.nn_budget, args.display)
+
+    """
+    python deep_sort_yolo3_video.py \
+    --video_file=./class.mp4 \
+    --min_confidence=0.1 \
+    --nn_budget=100 \
+    --display
+    """
